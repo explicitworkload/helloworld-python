934b17217d348d3f93ffe23780f6bd1b6398297c
explicitworkload <peterchenwy@gmail.com>
2023-10-17 03:35:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# helloworld-python
+let's make a change.
+1
+# hw-python
---
+++ test.py
@@ -0,0 +1,6 @@
+import pytest
+from helloworld import helloworld
+
+def test_testOne():
+  result = helloworld()
+  assert result == "hello world!"
